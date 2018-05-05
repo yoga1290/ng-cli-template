44acc01d5b774c2059659e3516cab08174a828aa
Youssef Gamil <yoga1290@gmail.com>
2018-05-05 17:31:32 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 GLOBAL_NODEJS_PACKAGES=$(npm root -g)
 COLLECTION_PATH='@custom/pug'
+
+# copy original templates to new collection
+sudo cp -R \
+  $GLOBAL_NODEJS_PACKAGES/@angular/cli/node_modules/@schematics/angular \
+  $GLOBAL_NODEJS_PACKAGES/@angular/cli/node_modules/$COLLECTION_PATH
+
+# overwrite/update templates:
 sudo cp -R . $GLOBAL_NODEJS_PACKAGES/@angular/cli/node_modules/$COLLECTION_PATH
