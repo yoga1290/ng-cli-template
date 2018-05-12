dc65bd3cff12af16ce872a2180b6e05dea31891b
Youssef Gamil <yoga1290@gmail.com>
2018-05-12 10:25:54 +0200

    HttpClientModule

---
+++ application/other-files/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+<% if (routing) { %>
+import { AppRoutingModule } from './app-routing.module';<% } %>
+<% if (serviceWorker) { %>
+import { ServiceWorkerModule } from '@angular/service-worker';<% } %>
+import { AppComponent } from './app.component';
+<% if (serviceWorker) { %>
+import { environment } from '../environments/environment';<% } %>
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule<% if (routing) { %>,
+    AppRoutingModule<% } %><% if (serviceWorker) { %>,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })<% } %>
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
