63c2e82ae08ac95b6373fa05b8798de8cb176477
Youssef Gamil <yoga1290@gmail.com>
2018-05-12 10:26:30 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 GLOBAL_NODEJS_PACKAGES=$(npm root -g)
-COLLECTION_PATH='@custom/pug'
+COLLECTION_PATH='@schematics/angular'
 
 # copy original templates to new collection
-sudo cp -R \
-  $GLOBAL_NODEJS_PACKAGES/@angular/cli/node_modules/@schematics/angular \
+sudo cp -fR \
+  $GLOBAL_NODEJS_PACKAGES/@angular/cli/node_modules/@schematics/angular/* \
   $GLOBAL_NODEJS_PACKAGES/@angular/cli/node_modules/$COLLECTION_PATH
 
 # overwrite/update templates:
