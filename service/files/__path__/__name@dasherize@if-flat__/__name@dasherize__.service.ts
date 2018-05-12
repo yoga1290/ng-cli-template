2597d28a8a52206174e98af7f36e5f8167aa3fa3
Youssef Gamil <yoga1290@gmail.com>
2018-05-12 04:09:19 +0200

    service

---
+++ service/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class <%= classify(name) %>Service {
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  get<%= classify(name) %> () : Observable<Array<Object>> {
+    return this.http.get<Array<Object>>("/<%= classify(name) %>").pipe(
+      // tap(_ => this.log(`fetched hero id=${id}`)),
+      // catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+
+  add<%= classify(name) %> (quote : Object) : Observable<Object> {
+    return this.http.post<Object>("/<%= classify(name) %>", quote, httpOptions).pipe(
+      // tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      // catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  update<%= classify(name) %> (id : String, quote : Object) : Observable<Object> {
+    return this.http.put<Object>("/<%= classify(name) %>/" + id, quote, httpOptions).pipe(
+      // tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      // catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  delete<%= classify(name) %> (id : String) : Observable<Object> {
+    return this.http.delete<Object>("/<%= classify(name) %>/" + id, httpOptions).pipe(
+      // catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+}
